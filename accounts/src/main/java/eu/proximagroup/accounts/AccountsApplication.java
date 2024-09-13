ce4064972a9ca8d5f444293d239a1b9d1dec6158
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2024-09-13 15:34:18 +0200

    refactor auditing

--- accounts/src/main/java/eu/proximagroup/accounts/AccountsApplication.java
+++ accounts/src/main/java/eu/proximagroup/accounts/AccountsApplication.java
@@ -2,8 +2,10 @@ package eu.proximagroup.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AccountsApplication {
 
 	public static void main(String[] args) {
