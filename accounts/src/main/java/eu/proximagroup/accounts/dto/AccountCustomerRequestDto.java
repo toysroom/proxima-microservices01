2c9aba2652a6324358976afece159569bfe138c4
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2024-09-16 14:34:56 +0200

    modifiche a respondeto

---
+++ accounts/src/main/java/eu/proximagroup/accounts/dto/AccountCustomerRequestDto.java
@@ -0,0 +1,33 @@
+package eu.proximagroup.accounts.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AccountCustomerRequestDto {
+
+	@NotEmpty(message = "account_number is required")
+	private String accountNumber;
+	
+    @NotEmpty(message = "account_type is required")
+    private String accountType;
+	
+    @NotEmpty(message = "branch_address is required")
+    private String branchAddress;
+	
+    @NotEmpty(message = "first_name is required")
+	private String firstName;
+
+    @NotEmpty(message = "last_name is required")
+	private String lastName;
+
+    @NotEmpty(message = "email is required")
+	@Email(message = "email is not correct")
+	private String email;
+
+    @NotEmpty(message = "mobile_number is required")
+	private String mobileNumber;
+
+}
--- accounts/src/main/java/eu/proximagroup/accounts/dto/ResponseErrorDto.java
+++ accounts/src/main/java/eu/proximagroup/accounts/dto/ResponseErrorDto.java
@@ -4,14 +4,23 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter @AllArgsConstructor
+@Getter @Setter
 public class ResponseErrorDto<T> {
 	private String apiPath;
+	private String errorMethod;
 	private HttpStatus errorCode;
 	private T errorMessage;
 	private LocalDateTime errorTime;
+	
+	public ResponseErrorDto(String apiPath, String errorMethod, HttpStatus errorCode, T errorMessage) 
+	{
+		this.apiPath = apiPath;
+		this.errorMethod = errorMethod;
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+		this.errorTime = LocalDateTime.now();
+	}
 }
