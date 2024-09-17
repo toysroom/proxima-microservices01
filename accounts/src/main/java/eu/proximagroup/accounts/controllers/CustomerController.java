25d7ce36ddc107e6e41d71dd67b194581fcc2d38
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2024-09-17 16:37:20 +0200

    modifiche customer

--- accounts/src/main/java/eu/proximagroup/accounts/controllers/CustomerController.java
+++ accounts/src/main/java/eu/proximagroup/accounts/controllers/CustomerController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import eu.proximagroup.accounts.constants.CustomerConstants;
--- accounts/src/main/java/eu/proximagroup/accounts/entities/Customer.java
+++ accounts/src/main/java/eu/proximagroup/accounts/entities/Customer.java
@@ -1,20 +1,26 @@
 package eu.proximagroup.accounts.entities;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
-@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
 @Entity
 @Table(name = "customers")
 public class Customer extends BaseEntity {
@@ -25,10 +31,12 @@ public class Customer extends BaseEntity {
 
 	@Column(name = "first_name")
     @NotEmpty(message = "first_name is required")
+	@Size(min=3, message = "first name must greater than {min} characters")
 	private String firstName;
 
 	@Column(name = "last_name")
     @NotEmpty(message = "last_name is required")
+	@Size(min=3, message = "last_name must greater than {min} characters")
 	private String lastName;
 
 	@Column(name = "email", unique = true)
@@ -38,6 +46,11 @@ public class Customer extends BaseEntity {
 
 	@Column(name = "mobile_number", unique = true)
     @NotEmpty(message = "mobile_number is required")
+	@Size(min=12, max=15, message = "mobile_number must between {min} e {max} characters")
+	@Pattern(regexp = "^(\\\\+|[0]{2})[0-9]{11,14}$", message = "mobile_number must have internationla prefix")
 	private String mobileNumber;
 	
+	@JsonManagedReference
+	@OneToMany(mappedBy="customer")
+	private List<Account> accounts;
 }
