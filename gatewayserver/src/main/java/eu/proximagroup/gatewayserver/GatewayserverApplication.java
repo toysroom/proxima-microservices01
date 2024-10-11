f27cc3803d48ee7290fc54cfbfc3422d1339ee0a
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2024-10-11 14:06:42 +0200

    update to gateway

---
+++ gatewayserver/src/main/java/eu/proximagroup/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,44 @@
+package eu.proximagroup.gatewayserver;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	
+	@Bean
+	public RouteLocator proximaRouteConfig(RouteLocatorBuilder routeLocatorBuilder)
+	{
+		
+		return routeLocatorBuilder.routes()
+			.route( p -> p.path("/proxima/accounts/**")
+					.filters( 
+							f -> f.rewritePath("/proxima/accounts/(?<segment>.*)","/${segment}")
+							.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+					).uri("lb://ACCOUNTS"))
+			
+			.route( p -> p.path("/proxima/cards/**")
+					.filters( 
+							f -> f.rewritePath("/proxima/cards/(?<segment>.*)","/${segment}")		
+							.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+					).uri("lb://CARDS"))
+			
+			.route( p -> p.path("/proxima/loans/**")
+					.filters( 
+							f -> f.rewritePath("/proxima/loans/(?<segment>.*)","/${segment}")
+							.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+					).uri("lb://LOANS"))
+						
+			.build();
+	}
+}
